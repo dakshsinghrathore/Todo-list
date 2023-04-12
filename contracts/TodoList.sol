6b180cd98cd9c80c40d55fd3956daf6eddb53ded
Daksh Singh Rathore <dakshrathore1729@gmail.com>
2023-04-12 05:46:18 +0530

    Project Setup

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,34 @@
 pragma solidity ^0.5.0;
 
-contract TodoList{
-uint public taskCount = 0; 
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
